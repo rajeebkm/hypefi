ddde901f329453f8c17d14ef33d01904c408fe0d
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2025-03-20 16:52:01 +0530

    fix:front-end

--- packages/nextjs/hooks/scaffold-eth/useSelectedNetwork.ts
+++
@@ -1,8 +0,0 @@
-import scaffoldConfig from "~~/scaffold.config";
-import { useGlobalState } from "~~/services/store/store";
-import { AllowedChainIds } from "~~/utils/scaffold-eth";
-
-export function useSelectedNetwork(chainId?: AllowedChainIds) {
-  const targetNetwork = useGlobalState(({ targetNetwork }) => targetNetwork);
-  return scaffoldConfig.targetNetworks.find(targetNetwork => targetNetwork.id === chainId) ?? targetNetwork;
-}
